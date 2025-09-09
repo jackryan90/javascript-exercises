190e91471584eec70d9aeffa21476fbbd7927e24
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-09 09:26:29 -0600

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  let results = arr.slice();
+  for (let arg of args) {
+    results = results.filter(item => item !== arg)
+  }
+  return results;
 };
 
 // Do not edit below this line
