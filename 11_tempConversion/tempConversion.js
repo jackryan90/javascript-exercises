fbea236843417cf2100d2150acf3b8822b8a1dcb
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-09 09:59:24 -0600

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let result = (tempInFahrenheit - 32) * 5 / 9;
+
+  const precision = 1;
+  const multiplier = Math.pow(10, precision);
+  return Math.round(result * multiplier) / multiplier;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let result = (tempInCelsius * 9 / 5) + 32;
+
+  const precision = 1;
+  const multiplier = Math.pow(10, precision);
+  return Math.round(result * multiplier) / multiplier;
 };
 
 // Do not edit below this line
