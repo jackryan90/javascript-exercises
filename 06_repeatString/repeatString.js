d11385b8c56d714e17fd0dfcec974a681d2bbfb8
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-08 22:22:01 -0600

    Complete 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, numRepeats) {
+  if (numRepeats < 0) {
+    return "ERROR";
+  }
 
+  let result = "";
+  for (let i = 0; i < numRepeats; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
