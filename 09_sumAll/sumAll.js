a172a20c47a1203a6d1aa42e6d9e8f8ae65a89f7
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-09 09:44:46 -0600

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  for (let arg of arguments) {
+    if ((typeof arg !== "number") || (arg < 0) || (!Number.isInteger(arg))) {
+      return "ERROR";
+    };
+  }
+  
+  let args = [num1, num2];
+  args.sort((a, b) => a - b);
 
+  let sum = 0;
+  for (let i = args[0]; i <= args[1]; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
