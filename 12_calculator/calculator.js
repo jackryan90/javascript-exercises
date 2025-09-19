998c73f31b4b9bcc6433e9c731deffccab5b3f84
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-19 17:23:13 -0600

    Complete calculator exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((runningSum, num) => runningSum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((runningProduct, num) => runningProduct * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
