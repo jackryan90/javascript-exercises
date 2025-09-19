c54d037ee240e249b945dbe1f80cd0446f436d60
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-19 17:54:29 -0600

    Complete fibonacci exercises

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  member = +member;
+  if (member < 0) return "OOPS";
 
+  let lastTwo = [0, 1];
+
+  if (member < 2) {
+    return lastTwo[member];
+  }
+
+  for (let i = 2; i < member; i++) {
+    const next = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = next;
+  }
+
+  return lastTwo[0] + lastTwo[1];
 };
 
 // Do not edit below this line
