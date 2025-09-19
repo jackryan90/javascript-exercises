37fa8337c028d266029fb732a1683157f2d2f0eb
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-19 17:41:11 -0600

    Complete palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const input = (
+    word
+    .toLowerCase()
+    .split("")
+    .filter(char => /[a-zA-Z0-9]/.test(char))
+    .join("")
+  );
+  const output = input.split("").toReversed().join("");
+  return input === output;
 };
 
 // Do not edit below this line
