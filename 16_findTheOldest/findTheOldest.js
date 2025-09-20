19ee8f1f7d5f35f9078c2f2331792e0cf68584e8
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-19 21:33:53 -0600

    Complete exercises for findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  people.sort((a, b) => {
+    const ages = [a, b].map((person) => {
+      if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+      }
+      const currentYear = new Date().getFullYear();
+      return currentYear - person.yearOfBirth;
+    });
+    return ages[0] - ages[1];
+  });
 
+  return people[people.length - 1];
 };
 
 // Do not edit below this line
